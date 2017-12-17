4ff38ed76b448bac585ba276ad605ad4112c407e
Devendra Kumar L <dkumarl@vmware.com>
2017-12-17 15:40:54 +0530

    Added Procfile to deploy to heroku and Updated package.json. Small code change in server.js.

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ var express = require('express');
 
 var app = express();
 
+app.set('port', process.env.PORT || 5001);
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+	res.send('Visit /bingo to play the game');
+});
 
 app.get('/bingo', function(req, res) {
 	res.render('pages/bingo');
 });
 
-app.listen(9001);
-
-console.log('game has started at http://localhost:9000/bingo');
+app.listen(app.get('port'), function () {
+	console.log('html bningo game is running on port: ', app.get('port'));
+});
